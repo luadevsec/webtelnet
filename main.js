8da14bd4d390bd8d6ad316c26a46ae8769b071d8
Lunna Cipher <lunna.devsec@gmail.com>
2024-06-09 14:10:27 -0300

    falta o utf8 ou ansi

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ var path = require('path'),
     http = require('http'),
     webtelnet = require('./webtelnet-proxy.js');
 
+// Inicialize a configuração do servidor
 var conf = {
   telnet: {
     host: '127.0.0.1',
@@ -23,12 +24,11 @@ var conf = {
   logTraffic: true,
 };
 
+// Processamento dos argumentos da linha de comando
 var argv = process.argv;
-var me = argv[1];
 var args = require('minimist')(argv.slice(2));
 
-process.stdout.write('webtelnet, version ' + info.version + ', by ' + info.author.name + ' <' + info.author.email +'>\n');
-
+// Verificação da sintaxe da linha de comando
 if(args._.length < 2) {
   process.stdout.write(
     'Syntax: webtelnet <http-port> <telnet-port> [options]\n' +
@@ -40,21 +40,27 @@ if(args._.length < 2) {
   process.exit(0);
 }
 
+// Configuração das portas do servidor
 conf.web.port = parseInt(args._[0], 10);
 conf.telnet.port = parseInt(args._[1], 10);
 
+// Definição do host do servidor Telnet
 if(args.h) conf.telnet.host = args.h;
+// Definição do diretório www
 if(args.w) conf.www = path.resolve(args.w);
 
+// Configuração do aplicativo Express
 var app = express().use(express.static(conf.www));
 var httpserver = http.createServer(app);
+
+// Inicialização do servidor HTTP
 httpserver.listen(conf.web.port, conf.web.host, function(){
   console.log('listening on ' + conf.web.host + ':' + conf.web.port);
 });
 
-// create socket io
+// Criação do servidor de WebSocket
 var io = socketio.listen(httpserver);
 
-// create webtelnet proxy and bind to io
+// Criação do proxy WebTelnet e associação ao WebSocket
 var webtelnetd = webtelnet(io, conf.telnet.port, conf.telnet.host);
 if(args.c) webtelnetd.setCharset(args.c);
--- www/main.js
+++ www/main.js
@@ -1,190 +1,25 @@
-/*
-function Utf8ArrayToStr(array) {
-    var out, i, len, c;
-    var char2, char3;
+$(document).ready(function(){ 
+  // Inicialize o terminal xterm.js
+  const terminal = new Terminal();
+  terminal.open(document.getElementById('terminal'));
 
-    out = "";
-    len = array.length;
-    i = 0;
-    while(i < len) {
-    c = array[i++];
-    switch(c >> 4)
-    { 
-      case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
-        // 0xxxxxxx
-        out += String.fromCharCode(c);
-        break;
-      case 12: case 13:
-        // 110x xxxx   10xx xxxx
-        char2 = array[i++];
-        out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
-        break;
-      case 14:
-        // 1110 xxxx  10xx xxxx  10xx xxxx
-        char2 = array[i++];
-        char3 = array[i++];
-        out += String.fromCharCode(((c & 0x0F) << 12) |
-                       ((char2 & 0x3F) << 6) |
-                       ((char3 & 0x3F) << 0));
-        break;
-    }
-    }
-
-    return out;
-}
-
-function binayUtf8ToString(buf, begin){
-  var i = 0;
-  var pos = 0;
-  var str ="";
-  var unicode = 0 ;
-  var flag = 0;
-  for (pos = begin; pos < buf.length;){
-    flag= buf[pos];
-    if ((flag >>>7) === 0 ) {
-      str+= String.fromCharCode(buf[pos]);
-      pos += 1;
-
-    }
-    else if ((flag &0xFC) === 0xFC ){
-      unicode = (buf[pos] & 0x3) << 30;
-      unicode |= (buf[pos+1] & 0x3F) << 24; 
-      unicode |= (buf[pos+2] & 0x3F) << 18; 
-      unicode |= (buf[pos+3] & 0x3F) << 12; 
-      unicode |= (buf[pos+4] & 0x3F) << 6;
-      unicode |= (buf[pos+5] & 0x3F);
-      str+= String.fromCharCode(unicode) ;
-      pos += 6;
-
-    }else if ((flag &0xF8) === 0xF8 ){
-      unicode = (buf[pos] & 0x7) << 24;
-      unicode |= (buf[pos+1] & 0x3F) << 18; 
-      unicode |= (buf[pos+2] & 0x3F) << 12; 
-      unicode |= (buf[pos+3] & 0x3F) << 6;
-      unicode |= (buf[pos+4] & 0x3F);
-      str+= String.fromCharCode(unicode) ;
-      pos += 5;
-
-    }
-    else if ((flag &0xF0) === 0xF0 ){
-      unicode = (buf[pos] & 0xF) << 18;
-      unicode |= (buf[pos+1] & 0x3F) << 12; 
-      unicode |= (buf[pos+2] & 0x3F) << 6;
-      unicode |= (buf[pos+3] & 0x3F);
-      str+= String.fromCharCode(unicode) ;
-      pos += 4;
-
-    }
-
-    else if ((flag &0xE0) === 0xE0 ){
-      unicode = (buf[pos] & 0x1F) << 12;;
-      unicode |= (buf[pos+1] & 0x3F) << 6;
-      unicode |= (buf[pos+2] & 0x3F);
-      str+= String.fromCharCode(unicode) ;
-      pos += 3;
-
-    }
-    else if ((flag &0xC0) === 0xC0 ){ //110
-      unicode = (buf[pos] & 0x3F) << 6;
-      unicode |= (buf[pos+1] & 0x3F);
-      str+= String.fromCharCode(unicode) ;
-      pos += 2;
-
-    }
-    else{
-      str+= String.fromCharCode(buf[pos]);
-      pos += 1;
-    }
- } 
- return str;
-}
-*/
-
-function writeToScreen(str) {
-  var out = $('div#out');
-  out.append('<span class="out">' + str + '</span>');
-  out.scrollTop(out.prop("scrollHeight"));
-}
-
-function writeServerData(buf) {
-  // now we send utf8 string instead of utf8 array
-  // var data = new Uint8Array(buf);
-  // var str = binayUtf8ToString(data, 0);
-  var str = buf;
-
-  var lines = str.split('\r\n');
-  for(var i=0; i<lines.length; i++) {
-    var line = lines[i].replace(/\s\s/g, '&nbsp;');
-    if(i < lines.length-1) line += '<br/>';
-
-    // replace the prompt "> " with a empty line
-    var len = line.length;
-    if(len>=2 && line.substr(len-2) == '> ') line = line.substr(0, line-2) + '<br/>';
-
-    line = ansi_up.ansi_to_html(line);
-
-    writeToScreen(line);
-  }
-}
-
-function adjustLayout() {
-  var w = $(window).width(), h = $(window).height();
-  var w0 = $('div#cmd').width();
-  var w1 = $('button#send').outerWidth(true);
-  var w2 = $('button#clear').outerWidth(true);
-  $('input#cmd').css({
-    width: (w0 - (w1+w2+14)) + 'px',
+  // Conecte-se ao servidor WebSocket
+  const socket = io.connect();
+  socket.on('connect', function() {
+    console.log('Connected to server');
   });
-  var h0 = $('div#cmd').outerHeight(true);
-  $('div#out').css({
-    width: (w-2) + 'px',
-    height: (h - h0 -2) + 'px',
+  socket.on('disconnect', function() {
+    console.log('Disconnected from server');
   });
-}
-
-$(window).resize(adjustLayout);
-
-$(document).ready(function(){
-  // websocket
-  var sock = io.connect();
-  sock.on('stream', function(buf){
-    writeServerData(buf);
-  });
-  sock.on('status', function(str){
-    writeToScreen(str);
-  });
-  sock.on('connected', function(){
-    console.log('connected');
-  });
-  sock.on('disconnect', function(){
-    console.log('disconnected');
+  
+  // Redirecione a entrada do terminal para o servidor Telnet
+  terminal.onData(data => {
+    socket.emit('stream', data);
   });
 
-  // send
-  var send = function(str) {
-    writeToScreen(str);
-    if(sock) sock.emit('stream', str);
-  }
-  var sendInput = function() {
-    var cmd = $('input#cmd');
-    send(cmd.val().trim() + '\n');
-    cmd.val('');
-  }
-
-  // UI events
-  $('input#cmd').keypress(function(e) {
-    if(e.keyCode == 13) sendInput();
-  });
-  $('button#send').click(function(e) {
-    sendInput();
-  });
-  $('button#clear').click(function(e) {
-    $('div#out').html('');
+  // Exiba a saída recebida do servidor no terminal xterm.js
+  socket.on('stream', function(data) {
+    terminal.write(data);
   });
 
-  setTimeout(function(){
-    adjustLayout();
-    
-    send('\n');
-  },200)
 });
